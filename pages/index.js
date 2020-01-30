27dfb4907d7487aff2a1bfaedf39f1ebc8668ddf
Bilal Budhani <bilalbudhani@gmail.com>
2020-01-30 21:44:01 +0530

    Adds document file for body styling

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ class Item extends Component {
     const { qty } = this.state;
     return (
         <div className="px-6 py-4 bg-white flex flex-col justify-center items-center rounded m-1">
-          <strong class="text-xl">{name}</strong>
+          <strong className="text-xl">{name}</strong>
 
           <span className="text-green-800 font-bold">₹ {cost.toLocaleString('en-IN')}</span>
 
@@ -70,24 +70,24 @@ class HomePage extends Component {
 
   render() {
     return (
-    <main className="bg-gray-200 flex w-full h-screen justify-center items-center p-6 lg:p-0 my-20 lg:my-0">
-      <div className="container max-w-3xl">
-        <header className="px-12 py-8 bg-white my-2">
-          <h1 className="font-bold text-3xl leading-tight text-center">What all can be done with this budget?</h1>
-        </header>
+        <main className="flex w-full h-screen justify-center items-center px-6 py-16 lg:p-0">
+          <div className="container max-w-3xl">
+            <header className="px-12 py-8 bg-white my-2">
+              <h1 className="font-bold text-3xl leading-tight text-center">What all can be done with this budget?</h1>
+            </header>
 
-        <section className="bg-green-500 px-12 py-4 text-white flex justify-center my-2">
-          <h3 className="font-bold text-2xl text-center">₹ {this.state.budget.toLocaleString('en-IN')} left</h3>
-        </section>
+            <section className="bg-green-500 px-12 py-4 text-white flex justify-center my-2">
+              <h3 className="font-bold text-2xl text-center">₹ {this.state.budget.toLocaleString('en-IN')} left</h3>
+            </section>
 
-        <section className="flex flex-1 flex-col lg:flex-row flex-wrap my-2">
-          <Item name="New University" cost={10000000} onBuild={this.handleBuild} onUndo={this.handleUndo} />
-          <Item name="Fix potholes" cost={2500000} onBuild={this.handleBuild} onUndo={this.handleUndo} />
-          <Item name="Generate new jobs" cost={4500000} onBuild={this.handleBuild} onUndo={this.handleUndo} />
-          <Item name="Improve health care" cost={6000000} onBuild={this.handleBuild} onUndo={this.handleUndo} />
-        </section>
-      </div>
-    </main>
+            <section className="flex flex-1 flex-col lg:flex-row flex-wrap my-2">
+              <Item name="New University" cost={10000000} onBuild={this.handleBuild} onUndo={this.handleUndo} />
+              <Item name="Fix potholes" cost={2500000} onBuild={this.handleBuild} onUndo={this.handleUndo} />
+              <Item name="Generate new jobs" cost={4500000} onBuild={this.handleBuild} onUndo={this.handleUndo} />
+              <Item name="Improve health care" cost={6000000} onBuild={this.handleBuild} onUndo={this.handleUndo} />
+            </section>
+          </div>
+        </main>
     );
   }
 }
