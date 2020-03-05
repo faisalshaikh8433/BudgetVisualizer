77c44de043924187bf2a5ab07039062aa0da2124
faisal <faisalshaikh8433@gmail.com>
2020-03-05 18:30:19 +0530

    updates name

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>
-          BudgetVisualizer - What all can be done with this huge amount of money
-        </title>
+        <title>Money Spender - Spend huge amount of money</title>
         <link
           href="https://fonts.googleapis.com/css?family=Muli:400,700,900&display=swap"
           rel="stylesheet"
